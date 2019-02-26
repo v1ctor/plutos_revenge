85e1954ca23b64bc37014020098e025b3231c6c0
Victor Buldakov <victor@buldakov.org>
2019-02-26 23:05:14 +0000

    hacks! hacks! hacks

---
+++ Assets/Scripts/UI/LoadSceneAfterTimeout.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class LoadSceneAfterTimeout : MonoBehaviour {
+
+    public float duration = 3f;
+    public string sceneName;
+
+    private float endTime;
+
+	// Use this for initialization
+	void Start () {
+        endTime = Time.time + duration;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (endTime < Time.time) {
+            StartCoroutine(EndScene());
+        }
+    }
+
+    IEnumerator EndScene() {
+        yield return new WaitForSeconds(1f);
+        GameSceneManager.instance.Load(sceneName);
+    }
+}
